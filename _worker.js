e3fd722c47cbb546891d6b49d294f341f8f40f27
disvpn <168737287+disvpn@users.noreply.github.com>
2024-05-07 07:45:33 +0330

    _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'f117f9f5-51f6-4ee3-aeaa-c0d5152e1272';
+let userID = 'f6c283bb-156a-4ae1-94cf-0e7b195a6a79';
 
 const พร็อกซีไอพีs = ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'workers.cloudflare.cyou'];
 
